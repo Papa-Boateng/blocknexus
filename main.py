da172b46688c624c6b9673c3dd982024e8c2318a
Papa Boateng <papaboateng57@gmail.com>
2023-04-22 04:29:23 +0000

    Update Main

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ bot = telebot.TeleBot(config.API_TOKEN)
 server = Flask(__name__)
 
 
-#bot. infinity_polling()
+#At flask
+@server.route('/', methods=['GET', 'HEAD'])
+def index():
+    return 'Blocknexus Bot active!', 200
+
 @server.route('/' + config.API_TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
+#tele webhook config
+bot.remove_webhook()
+bot.set_webhook(url='https://fqfmrvssaa7wf6hg7t3fypz6dq.srv.us/' + config.API_TOKEN)
+#server
+server.run(port=8000, debug=True)
